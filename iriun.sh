a34b615073c812e44f12d61b0c18411f4fa4c2d7
geraldohomero <geraldohomero@protonmail.com>
2022-08-05 18:09:50 -0300

    edit sh file

--- iriun.sh
+++ iriun.sh
@@ -1,2 +1,24 @@
-sudo rmmod v4l2loopback && sudo modprobe v4l2loopback exclusive_caps=1
+#!/usr/bin/env bash
+VERMELHO='\e[1;91m'
+VERDE='\e[1;92m'
+SEM_COR='\e[0m'
 
+# Internet?
+if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+  echo -e "${VERMELHO}[ERROR] - Not connected.${SEM_COR}"
+  exit 1
+else
+  echo -e "${VERDE}[INFO] - Connected ${SEM_COR}"
+fi
+
+
+procedimento () {
+  echo -e "${VERDE}[INFO] - rmmod v4l2loopback ${SEM_COR}"
+sudo rmmod v4l2loopback
+  echo -e "${VERDE}[INFO] - modprobe v4l2loopback exclusive_caps=1 ${SEM_COR}"
+sudo modprobe v4l2loopback exclusive_caps=1
+}
+
+
+#----# Execução
+procedimento
