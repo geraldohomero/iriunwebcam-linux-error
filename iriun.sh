54ef46c3d5c4545cd1051832ea3f0e0b476e9087
Geraldo Homero <geraldohomero@protonmail.com>
2024-06-07 19:45:37 -0300

    Update iriun.sh

--- iriun.sh
+++ iriun.sh
@@ -4,7 +4,7 @@ VERDE='\e[1;92m'
 SEM_COR='\e[0m'
 
 procedimento () {
-  echo -e "${VERDE}[INFO] - rmmod v4l2loopback ${SEM_COR}"
+  echo -e "${VERDE}[INFO] - asdf v4l2loopback ${SEM_COR}"
 sudo rmmod v4l2loopback
   echo -e "${VERDE}[INFO] - modprobe v4l2loopback exclusive_caps=1 ${SEM_COR}"
 sudo modprobe v4l2loopback exclusive_caps=1
